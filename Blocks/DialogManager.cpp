a07c10202226bac0b2d2c2748dc361ee35c96017
Salinder Sidhu <salinder.sid@gmail.com>
2016-01-06 11:34:33 -0500

    Fixed dialog bug
    Resolved dialog issue in linux, message dialogs will not cut off message text

--- Blocks/DialogManager.cpp
+++ Blocks/DialogManager.cpp
@@ -15,7 +15,7 @@ void DialogManager::message(string message) {
 #ifdef _WIN32
 	MessageBoxA(windowHandle, message.c_str(), dialogTitle.c_str(), MB_ICONINFORMATION | MB_OK);
 #elif __linux__
-	system(("zenity --info --title=" + dialogTitle + " --text=" + message).c_str());
+	system(("zenity --info --title=" + dialogTitle + " --text='" + message + "'").c_str());
 #endif
 }
 
@@ -23,6 +23,6 @@ void DialogManager::error(string message) {
 #ifdef _WIN32
 	MessageBoxA(windowHandle, message.c_str(), dialogTitle.c_str(), MB_ICONERROR | MB_OK);
 #elif __linux__
-	system(("zenity --error --title=" + dialogTitle + " --text=" + message).c_str());
+	system(("zenity --error --title=" + dialogTitle + " --text='" + message + "'").c_str());
 #endif
 }
