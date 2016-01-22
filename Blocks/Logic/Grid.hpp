7d3f9f43ebe74445467060d5bceb720e720e19c7
Salinder Sidhu <salinder.sid@gmail.com>
2016-01-22 01:40:32 -0500

    added Grid header file

---
+++ Blocks/Logic/Grid.hpp
@@ -0,0 +1,46 @@
+#ifndef GRID_HPP
+#define GRID_HPP
+
+#include <string>
+#include <vector>
+#include "Block.hpp"
+using namespace std;
+
+class Grid {
+    public:
+        // Constructor and destructor
+        Grid(float, float, float, float, float, float);
+        virtual ~Grid() {};
+        // Configure Grid functions
+        void addBlock(Block);
+        void selectBlock(float, float);
+        void releaseBlock(&int);
+        void moveBlock(float, float);
+        void clear();
+        // Get Grid attribute functions
+        vector<Block> getBlocks();
+        bool isComplete();
+    private:
+        // Grid functions
+        float alignValue(float, float, float);
+        bool isHovering(float, float. float. float. float. float);
+        bool isNotCollision(Block, float, float);
+        bool isBounded(float, float, float, float);
+        // Grid variables
+        vector<Block> blocks;
+        float x;
+        float y;
+        float width;
+        float height;
+        float completeX;
+        float completeY;
+        // Calculation variables for Blocks
+        float prevX;
+        float prevY;
+        float distX;
+        float distY;
+        bool isBlockMoved;
+        bool isBlockFree;
+};
+
+#endif // GRID_HPP
